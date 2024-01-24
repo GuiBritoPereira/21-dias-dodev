9b0ec24424333ebbcbf25020f44d87d93356cbd1
Guilherme Brito <pereiraguilherme541@gmail.com>
2024-01-24 19:01:21 -0300

    pasta de aprendizado - Switch Case

---
+++ dia-5/index.js
@@ -0,0 +1,26 @@
+let idade = Number(prompt('insira sua idade'))
+
+switch(idade){
+  case 10:
+    console.log('voce tem 10 anos')
+    break;
+
+
+  case 20:
+    console.log('voce tem 20 anos')
+    break;
+
+
+    case 30:
+      console.log('voce tem 30 anos')
+      break;
+
+      default:
+        console.log('default')
+        break;
+
+}
+  
+
+
+  
