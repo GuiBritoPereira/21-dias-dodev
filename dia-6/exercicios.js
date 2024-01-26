c7be9ad7e17325db8a63e7ab7dfcaf29d6ff3e07
Guilherme Brito <pereiraguilherme541@gmail.com>
2024-01-25 21:26:17 -0300

    Aprendizado novo - Laço de repetição FOR

---
+++ dia-6/exercicios.js
@@ -0,0 +1,22 @@
+// criando variaveis 
+
+let numero = parseInt(prompt("Digite um numero inteiro e positivo"))
+
+// criando loop for
+
+for(let i = 0; i <= numero; i++){
+  console.log(i);
+}
+
+// criando loop for 
+
+for(let i = 0; i <= 50; i += 5){
+  console.log(i);
+}
+
+//contagem inversa
+
+  for (let i = numero; i >= 0; i--){
+    console.log(i)
+  }
+  
---
+++ dia-6/index.js
@@ -0,0 +1,5 @@
+let tabuada = Number(prompt("Escolha a tabuada"))
+
+for(let contador = 0; contador <= 100; contador++){
+  console.log(tabuada + "*" + contador + " = " + tabuada*contador) 
+}
