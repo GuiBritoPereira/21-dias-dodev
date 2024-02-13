b874f8fdfc63fbd8243618a362cc7f4312fbd08e
Guilherme Brito Pereira <pereiraguilherme541@gmail.com>
2024-02-13 00:13:02 -0300

    Colocando em prática - Objetos

---
+++ dia-13/index.js
@@ -0,0 +1,25 @@
+class Computador {
+    constructor(tipo, processador, video, armazenamento, memoriaRam, ssd) {
+        this.tipo = tipo;
+        this.processador = processador;
+        this.video = video;
+        this.armazenamento = armazenamento;
+        this.memoriaRam = memoriaRam;
+        this.ssd = ssd;
+    }
+
+    exibirInformacoes() {
+        console.log("Informações do Computador:");
+        console.log(`Tipo: ${this.tipo}`);
+        console.log(`Processador: ${this.processador}`);
+        console.log(`Vídeo: ${this.video}`);
+        console.log(`Armazenamento: ${this.armazenamento} GB`);
+        console.log(`Memória RAM: ${this.memoriaRam} GB`);
+        console.log(`SSD: ${this.ssd}`);
+    }
+}
+
+// EXEMPLO DE USO
+
+const meuComputador = new Computador("notebook", "Ryzen 7", "Dedicado", 1000, 16, true);
+meuComputador.exibirInformacoes();
